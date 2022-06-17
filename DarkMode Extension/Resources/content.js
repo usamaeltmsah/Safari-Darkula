e2da68fd96d50f62fb41b65fb7f8605cac1e36f6
usamaeltmsah <usamaeltmsah@gmail.com>
2022-06-18 00:59:44 +0200

    Change the page to dark mode when dark button clicked, and to light mode (Page mode) when the white button clicked

--- DarkMode Extension/Resources/content.js
+++ DarkMode Extension/Resources/content.js
@@ -5,3 +5,120 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+
+// var allElements = document.querySelectorAll("*");
+
+// for (var i=0, max=allElements.length; i < max; i++) {
+//     if (allElements[i] == document.body) {
+// //        document.write(1);
+//         continue;
+//     }
+// //    document.write(2);
+//     let color = window.getComputedStyle(allElements[i], null).getPropertyValue('color');
+//     let hex_color = rgba2hex(color);
+    
+//     let back_color = window.getComputedStyle(allElements[i], null).getPropertyValue('background-color');
+//     let background_hex_color = rgba2hex(back_color);
+// //    document.write(hex_color);
+//     let inverted_color = invertColor(hex_color, false);
+//     allElements[i].style.color = inverted_color;
+    
+// //    let inverted_back_color = invertColor(background_hex_color, false);
+// //    allElements[i].style.background = inverted_back_color
+    
+// //    var before = window.getComputedStyle(allElements[i], ':before');
+// //    var after = window.getComputedStyle(allElements[i], ':after');
+// //    if(before.content){
+// //        // found :before
+// //        console.log(before.content);
+// //    }
+// //    if(after.content){
+// //        // found :after
+// //        console.log(after.content);
+// //    }
+// }
+
+
+//let dark_color1 = "#262626"
+//const changeColor = () => {
+////    let background_color = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
+////    let background_hex_color = rgba2hex(background_color);
+////    let inverted_color = invertColor("#df00d5", false);
+//    document.querySelector("body").style.background = dark_color1;
+//    document.querySelector("#content").style.background = dark_color1
+//    document.querySelector('table.infobox').style.background = dark_color1
+//}
+//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//    if(request.color) {
+//        color = request.color
+//        changeColor()
+//    }
+//});
+
+//var allElements = document.querySelectorAll("*");
+//
+//for (var i=0, max=allElements.length; i < max; i++) {
+//    console.log(i);
+//    if (allElements[i] == document.body) {
+//        continue;
+//    }
+//    let color = window.getComputedStyle(document.allElements[i], null)
+//    let inverted_color = invertColor(color, false);
+//    document.querySelector(allElements[i]).style.background = inverted_color
+//    var before = window.getComputedStyle(allElements[i], ':before');
+//    var after = window.getComputedStyle(allElements[i], ':after');
+//    if(before.content){
+//        // found :before
+//        console.log(before.content);
+//    }
+//    if(after.content){
+//        // found :after
+//        console.log(after.content);
+//    }
+//}
+//
+//function rgba2hex(orig) {
+//  var a, isPercent,
+//    rgb = orig.replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+),?([^,\s)]+)?/i),
+//    alpha = (rgb && rgb[4] || "").trim(),
+//    hex = rgb ?
+//    (rgb[1] | 1 << 8).toString(16).slice(1) +
+//    (rgb[2] | 1 << 8).toString(16).slice(1) +
+//    (rgb[3] | 1 << 8).toString(16).slice(1) : orig;
+//
+//  return "#" + hex;
+//}
+//
+//function invertColor(hex, bw) {
+//    if (hex.indexOf('#') === 0) {
+//        hex = hex.slice(1);
+//    }
+//    // convert 3-digit hex to 6-digits.
+//    if (hex.length === 3) {
+//        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+//    }
+//    if (hex.length !== 6) {
+//        throw new Error('Invalid HEX color.');
+//    }
+//    var r = parseInt(hex.slice(0, 2), 16),
+//        g = parseInt(hex.slice(2, 4), 16),
+//        b = parseInt(hex.slice(4, 6), 16);
+//    if (bw) {
+//        // https://stackoverflow.com/a/3943023/112731
+//        return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+//            ? '#000000'
+//            : '#FFFFFF';
+//    }
+//    // invert color components
+//    r = (255 - r).toString(16);
+//    g = (255 - g).toString(16);
+//    b = (255 - b).toString(16);
+//    // pad each with zeros and return
+//    return "#" + padZero(r) + padZero(g) + padZero(b);
+//}
+//
+//function padZero(str, len) {
+//    len = len || 2;
+//    var zeros = new Array(len).join('0');
+//    return (zeros + str).slice(-len);
+//}
--- DarkMode Extension/Resources/popup.js
+++ DarkMode Extension/Resources/popup.js
@@ -1,1 +1,70 @@
-console.log("Hello World!", browser);
+//popup.js
+const colors = [{
+//    name: "Pink",
+//    code: '#FA96A7'
+//},{
+//    name: "Orange",
+//    code: '#F28500'
+//},{
+    name: "White",
+    code: '#FFFFFF'
+},{
+    name: "Dark",
+    code: '#A2A9B1'
+}]
+
+const changeMode = async (color) => {
+    const [tab] = await browser.tabs.query({currentWindow: true, active: true})
+    if (color == colors[0].code) {
+        let removingCSS = browser.tabs.removeCSS(tab.id, { file: "dark.css" });
+        removingCss.then(null, onError);
+    } else {
+        try {
+            let insertingCSS = browser.tabs.insertCSS(tab.id, { file: "dark.css" });
+            insertingCSS.then(null, onError);
+          } catch (err) {
+            console.error(`failed to insert CSS: ${err}`);
+          }
+    }
+    
+}
+
+colors.forEach(color => {
+    const button = document.createElement('button')
+    button.style.background = color.code
+    button.innerText = color.name
+    document.querySelector('#color-container').appendChild(button)
+    button.addEventListener('click', e => {
+        changeMode(color.code)
+    })
+})
+
+////var myParent = document.querySelector('#color-container');
+////var selectList = document.createElement("select");
+////selectList.id = "mySelect";
+////myParent.appendChild(selectList);
+
+//function swapStyleSheet(sheet) {
+//    document.getElementById('pagestyle').setAttribute('href', sheet);
+//}
+//
+//function toggleRef(ref) {
+//    // var blackCss = document.createElement('link');
+//    // blackCss.ref = "styles/dark.css";
+//    // blackCss.rel = "stylesheet";
+//    // document.head.appendChild(blackCss);
+//
+//    if ($('#pagestyle').length === 0) { // does not yet exist
+//
+//        $('<link />', {
+//            id: 'pagestyle',
+//            rel: 'stylesheet',
+//            href: ref
+//        }).appendTo('head');
+//
+//    } else { // exists, remove it
+//
+//        $('#pagestyle').remove();
+//
+//    }
+//}
