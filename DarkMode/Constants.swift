da762d7ca2d9efe6e4206b2476fe0d439b13a818
usamaeltmsah <usamaeltmsah@gmail.com>
2022-06-27 18:17:39 +0200

    Make design of the main app of the extension

---
+++ DarkMode/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  DarkMode
+//
+//  Created by Usama Fouad on 27/06/2022.
+//
+
+import Foundation
+
+struct K {
+    static let themeKey = "theme"
+    static let isOnIndexKey = "isOnSegment"
+    
+    static let autoDescText = "Your website theme will change when mode is enabled"
+    static let onDescText = "Your website theme will always be in that mode"
+    static let offDescText = "The theme will be turn off unless you enable for specific website"
+    
+    struct Themes {
+        static let darkTheme = "Dark"
+        static let grayTheme = "Gray"
+        static let sepiaTheme = "Sepia"
+    }
+}
--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -9,12 +9,37 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHandler {
-
+    @IBOutlet weak var changeThemeButton: UIButton!
+    @IBOutlet weak var colorLabel: UILabel!
     @IBOutlet var webView: WKWebView!
-
+    @IBOutlet weak var modeDescLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    var dark, gray, sepia: UIMenuElement!
+    var defaults: UserDefaults?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        defaults = UserDefaults.standard
+        
+        if let theme = defaults?.string(forKey: K.themeKey) {
+            switch theme {
+            case K.Themes.grayTheme:
+                self.updateThemeData(theme: theme, color: .gray)
+            case K.Themes.sepiaTheme:
+                self.updateThemeData(theme: theme, color: .systemBrown)
+            default:
+                self.updateThemeData(theme: theme, color: .black)
+            }
+        }
+        
+        if let isOnIndex = defaults?.integer(forKey: K.isOnIndexKey) {
+            segmentedControl.selectedSegmentIndex = isOnIndex
+            changeDescText(with: isOnIndex)
+        }
+        
         self.webView.navigationDelegate = self
         self.webView.scrollView.isScrollEnabled = false
 
@@ -28,7 +53,40 @@ class ViewController: UIViewController, WKNavigationDelegate, WKScriptMessageHan
     }
 
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        // Override point for customization.
+        print(message.name)
+//        if message.name == "test", let messageBody = message.body as? String {
+//            print(messageBody)
+//        }
     }
 
+    // Send the theme to safari extension
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.navigationType == .linkActivated {
+            if let url = navigationAction.request.url, url.scheme == "darkmode" {
+                let theme = url.host ?? ""
+                self.updateThemeData(theme: theme, color: .black)
+                decisionHandler(.cancel)
+            } else {
+                decisionHandler(.allow)
+            }
+        } else {
+            decisionHandler(.allow)
+        }
+    }
+    
+    func createJsonForJavaScript(for data: [String : Any]) -> String {
+        var jsonString : String?
+        do {
+           let jsonData = try JSONSerialization.data(withJSONObject: data,       options: .prettyPrinted)
+          // here "jsonData" is the dictionary encoded in JSON data .
+          jsonString = String(data: jsonData, encoding: .utf8)!
+           
+           jsonString = jsonString?.replacingOccurrences(of: "\n", with: "").replacingOccurrences(of: "\\", with: "")
+            } catch {
+                print(error.localizedDescription)
+        }
+        print(jsonString!)
+        return jsonString!
+    }
+    
 }
